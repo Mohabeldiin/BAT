33b9354a7ae718d7315616df3ce18d5e1c182102
Mohab Mohsen <hobamphab2000@gmail.com>
2021-09-05 17:05:17 +0200

    init Bat.bat

---
+++ BAT.bat
@@ -0,0 +1,35 @@
+cls
+ 
+@ECHO OFF
+ 
+title BAT
+ 
+if EXIST "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" goto UNBAT
+ 
+if NOT EXIST BAT goto BATER
+
+ren BAT "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+ 
+attrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+ 
+goto end
+
+:BATER
+
+MD BAT
+
+goto end
+ 
+:UNBAT
+ 
+set/p "pass=>"
+ 
+if NOT %pass%==3484 goto end
+ 
+attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+ 
+ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" BAT
+ 
+:end
+
+:End
